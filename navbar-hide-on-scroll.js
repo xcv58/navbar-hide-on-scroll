1fb2db74879d2413a043beb7df02f62eb3d24e35
xcv58 <chenyihonglove@gmail.com>
2015-07-20 13:54:20 -0400

    add scroll down hide navbar, scroll up show navbar

--- navbar-hide-on-scroll.js
+++ navbar-hide-on-scroll.js
@@ -1,19 +1,40 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Meteor.startup(function() {
+    $(window).scroll(function(event){
+      Session.set('didScroll', true);
+    });
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.nav.onCreated(function() {
+    Deps.autorun(function() {
+      if(Session.get('didScroll')) {
+        hasScrolled();
+        Session.set('didScroll', false);
+      }
+    });
   });
+
+  // Hide Header on on scroll down
+  // inspired by https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c
+  var lastScrollTop = 0;
+  var delta = 32;
+
+  function hasScrolled() {
+    var st = Math.max($(this).scrollTop(), 0);
+
+    if(Math.abs(lastScrollTop - st) <= delta) {
+      return;
+    }
+
+    if (st > lastScrollTop) {
+      $('.navbar').fadeOut(500);
+    } else {
+      $('.navbar').show();
+    }
+
+    lastScrollTop = st;
+  }
 }
 
 if (Meteor.isServer) {
